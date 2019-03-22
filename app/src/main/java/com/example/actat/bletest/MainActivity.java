0268a1fb62340da3fc827f4b41d48d7202639a1a
Actat <akiotoshima@gmail.com>
2019-03-22 11:39:21 +0900

    enable BLE

--- app/src/main/java/com/example/actat/bletest/MainActivity.java
+++ app/src/main/java/com/example/actat/bletest/MainActivity.java
@@ -1,13 +1,72 @@
 package com.example.actat.bletest;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_ENABLEBLUETOOTH = 1;// Bluetooth 有効化要求時の識別コード
+
+    private BluetoothAdapter mBluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Android端末がBLEをサポートしてるかの確認
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, R.string.ble_is_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+        // Bluetoothアダプタの取得
+        BluetoothManager bluetoothManager = (BluetoothManager)getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+        if (mBluetoothAdapter == null) {
+            // 端末がBluetoothをサポートしていない
+            Toast.makeText(this, R.string.bluetooth_is_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        requestBluetoothFeature();
+    }
+
+    private void requestBluetoothFeature() {
+        if (mBluetoothAdapter.isEnabled()) {
+            return;
+        }
+        // 有効化要求を行う
+        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enableBtIntent, REQUEST_ENABLEBLUETOOTH);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_ENABLEBLUETOOTH:
+                if (Activity.RESULT_CANCELED == resultCode) {
+                    // 有効化されなかった
+                    Toast.makeText(this, R.string.bluetooth_is_not_working, Toast.LENGTH_SHORT).show();
+                    finish();
+                    return;
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
